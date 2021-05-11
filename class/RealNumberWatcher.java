0ee2e69d27aad204da0d820bd6ccd118c50d55b0
bluelul <bluelul@gmail.com>
2021-05-11 22:47:19 +0700

    RealNumberWatcher separator

--- class/RealNumberWatcher.java
+++ class/RealNumberWatcher.java
@@ -5,13 +5,24 @@ import android.widget.EditText;
 
 public class RealNumberWatcher implements TextWatcher {
     private final EditText editText;
-
+    private char separator = ',';
+    
     /**
-     * Decimal-Signed inputType TextWatcher with separator adding automatically
+     * Decimal-Signed inputType TextWatcher with thousands separator adding automatically
      * @param editText EditText object
      */
     public RealNumberWatcher(EditText editText) {
         this.editText = editText;
+    }
+
+    /**
+     * Decimal-Signed inputType TextWatcher with thousands separator adding automatically
+     * @param editText EditText object
+     * @param separator Thousands separator character (default ',')
+     */
+    public RealNumberWatcher(EditText editText, char separator) {
+        this.editText = editText;
+        this.separator = separator;
     }
 
     @Override
@@ -27,24 +38,24 @@ public class RealNumberWatcher implements TextWatcher {
         // get cursor position without adding separator
         int realCursorPosition = 0;
         for (int i = 0; i < start + count; i++) {
-            if (inputStr.charAt(i) != ',') {
+            if (inputStr.charAt(i) != separator) {
                 realCursorPosition += 1;
             }
         }
 
         // re-draw separator ","
-        String postText = inputStr.replaceAll(",", "");
+        String postText = inputStr.replaceAll(String.valueOf(separator), "");
         int dotPosition = postText.concat(".").indexOf('.');
         int offsetForSeparator = (postText.indexOf('-') != -1) ? 2 : 1;
         for (int i = 0; i < (dotPosition - offsetForSeparator) / 3; i++) {
             postText = new StringBuilder(postText)
-                    .insert(dotPosition - (i+1)*3, ",").toString();
+                    .insert(dotPosition - (i+1)*3, separator).toString();
         }
 
         // update cursor position
         int cursorPosition = 0;
         for (int i = 0; i < realCursorPosition; i++) {
-            if (postText.charAt(cursorPosition) == ',') {
+            if (postText.charAt(cursorPosition) == separator) {
                 cursorPosition += 1;
             }
             cursorPosition += 1;
